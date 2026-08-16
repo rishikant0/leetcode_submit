575801f0dd523be33bcb4e5796d32dd597fa7c5f
rishikant0 <155719551+rishikant0@users.noreply.github.com>
2026-08-16 20:34:13 +0530

    Time: 4 ms (75.68%), Space: 114.6 MB (82.43%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.java
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -5,14 +5,12 @@ class Solution {
             count[s % 3]++;
         }
 
-        // If no type 1 or type 2 stones, Alice cannot win
         if (count[1] == 0 && count[2] == 0) return false;
 
-        // If count[0] is even, Alice wins if both type 1 and type 2 exist
+        
         if (count[0] % 2 == 0) {
             return count[1] > 0 && count[2] > 0;
         } else {
-            // If count[0] is odd, Alice wins if one type dominates the other by more than 2
             return Math.abs(count[1] - count[2]) > 2;
         }
     }
